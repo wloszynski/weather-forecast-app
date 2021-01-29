8ce895f7e8514743c7129574c2dd3dae28253c34
wloszynski <adrian@wloszynski.pl>
2021-01-29 13:34:39 +0100

    adding Weather class

--- js/app.js
+++ js/app.js
@@ -1,14 +1,79 @@
-const logo = document.querySelector("#logo");
-const aside = document.querySelector(".aside");
+"use strict";
 
-console.log(aside);
+import { API_KEY } from "./config.js";
 
-logo.addEventListener("click", () => {
-  // background-image: url("../img/windows_wallpaper.jpg");
-
-  if (aside.style.backgroundImage === 'url("../img/windows_wallpaper.jpg")') {
-    aside.style.backgroundImage = 'url("../img/sky.svg")';
-  } else {
-    aside.style.backgroundImage = 'url("../img/windows_wallpaper.jpg")';
+class Weather {
+  constructor(location, temp, clouds, chanceOfRain) {}
+}
+class WeatherWidget extends Weather {
+  constructor(
+    location,
+    temp,
+    clouds,
+    chanceOfRain,
+    feelsLike,
+    sunset,
+    airQuality
+  ) {
+    super(location, temp, clouds, chanceOfRain);
   }
-});
+}
+
+class App {
+  constructor() {
+    this.getPosition();
+  }
+
+  // getting user position
+  getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this.loadData.bind(this),
+        function () {
+          alert("Could not get your position");
+        }
+      );
+  }
+
+  loadData(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    let airQuality;
+    this.getAirQualityData(latitude, longitude).then((quality) => {
+      airQuality = quality;
+      console.log(airQuality);
+    });
+  }
+
+  getWeatherInfo(lat, lng) {}
+
+  getAirQualityData(lat, lng) {
+    // fetching air quality
+    return fetch(
+      `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => data.list[0].main.aqi)
+      .then((quality) => {
+        switch (quality) {
+          case 1:
+            return "Good";
+          case 2:
+            return "Fair";
+          case 3:
+            return "Moderate";
+          case 4:
+            return "Poor";
+          case 5:
+            return "Very Poor";
+
+          default:
+            return "Moderate";
+        }
+      })
+      .catch((err) => console.error(err));
+  }
+}
+
+new App();
