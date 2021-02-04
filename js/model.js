a1d4887747702b6445728d6e63354db752a80b9e
wloszynski <adrian@wloszynski.pl>
2021-02-04 13:16:45 +0100

    loadData and loadCityName transferred from app to model

--- js/model.js
+++ js/model.js
@@ -5,6 +5,19 @@ import {
   LIQ_API_URL,
 } from "./config.js";
 
+import * as weather from "./weather/weather";
+import * as view from "./view/view";
+import * as util from "./util/utility";
+
+// VARIABLES FOR WEATHER FORECAST
+const forecastContainer = document.querySelector(
+  ".weather-information__details"
+);
+
+// VARIABLES FOR WIDGET
+const widget = document.querySelector(".widget");
+
+// Loading Cities' names for search suggestions
 export const loadCities = async function () {
   return fetch(
     `https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json`
@@ -16,6 +29,7 @@ export const loadCities = async function () {
     .catch((err) => console.error(err));
 };
 
+// Get coords from given city name
 export const getCoordsFromCityName = async function (cityName) {
   return await fetch(
     `${LIQ_API_URL}/search.php?key=${LIQ_API_KEY}&format=json&q=${cityName}`
@@ -52,6 +66,7 @@ export const getCityName = async function (lat, lng) {
     .catch((err) => console.error(err));
 };
 
+// Get weather data for given lat and lng
 export const getWeatherData = async function (lat, lng) {
   return await fetch(
     `${OWM_API_URL}/onecall?lat=${lat}&lon=${lng}&appid=${OWM_API_KEY}&units=metric`
@@ -89,4 +104,34 @@ export const getAirQualityData = async function (lat, lng) {
       }
     })
     .catch((err) => console.error(err));
+};
+
+// Loading data -> weather, location, forecast,
+export const loadData = async function (lat, lng) {
+  util.loadingSpinnerInElement(forecastContainer);
+
+  util.hideElementOpacity(widget);
+
+  util.removeActiveClassFromImages();
+
+  const location = await getCityName(lat, lng).then((loc) => loc);
+
+  const [airQuality, weatherData] = await Promise.all([
+    await getAirQualityData(lat, lng).then((quality) => quality),
+    await getWeatherData(lat, lng),
+  ]);
+
+  // Display data in widget
+  view.displayWidget(
+    weather.getWeatherWidgetObject(weatherData, location, airQuality)
+  );
+
+  // Display data in forecast information
+  view.displayForecasts(weatherData);
+};
+
+// Getting location using from city name
+export const loadDataFromCityName = async function (cityName) {
+  const coords = await getCoordsFromCityName(cityName);
+  await loadData(...coords);
 };
