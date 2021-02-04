ee0b83baac5220ebe248ef5bd89fb77a4517ef0b
wloszynski <adrian@wloszynski.pl>
2021-02-04 03:24:49 +0100

    propper OWM API KEY

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 // OPENWEATHERMAP KEY
-export const OWM_API_KEY = "c7b5912d4d574f33e4e2e940d08f6f51";
-// export const OWM_API_KEY = "e634467f1f549dec38c97e5fa6acf975";
+// export const OWM_API_KEY = "c7b5912d4d574f33e4e2e940d08f6f51";
+export const OWM_API_KEY = "e634467f1f549dec38c97e5fa6acf975";
+
 // OPENWEATHERMAP API URL
 export const OWM_API_URL = "https://api.openweathermap.org/data/2.5";
 
